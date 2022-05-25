09f2abb778f2af1bbfce99fe3614c3784cecf023
Rishat <krp0697@gmail.com>
2022-05-25 18:46:04 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import Templates_2_2.BinOps;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1010", "1011"));
+        System.out.println(bins.mult("1010", "1111"));
+    }
+}
---
+++ src/main/java/Templates_2_2/BinOps.java
@@ -0,0 +1,10 @@
+package Templates_2_2;
+public class BinOps {
+    public String sum(String a, String b) {
+        return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
+    }
+
+    public String mult(String a, String b) {
+        return Integer.toBinaryString(Integer.parseInt(a, 2) * Integer.parseInt(b, 2));
+    }
+}
